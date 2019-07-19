1473c781cd1f9797e6784b33b0fdc90e497704ca
the-bose <sibibose01@gmail.com>
2019-07-19 15:44:23 +0530

    Image API integration

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from scraper import scrape
+from urllib.parse import quote_plus
 import requests
 import json
 
@@ -8,8 +9,8 @@ app = Flask(__name__)
 #STORE CONVERSION
 data = {}
 
-#INITIAL API CALL AND STORE CONVERSION
-def apiInit():
+#API CALL TO STORE CONVERSION
+def apiConvert():
     global data
 
     #API SETUP
@@ -23,6 +24,29 @@ def apiInit():
     #REQUEST
     resp = requests.get(finalURL, headers = headers)
     data = resp.json()
+
+#API CALL TO GET IMAGE
+def apiImage(title):
+    #CONVERTING TITLE TO URL FRIENDLY
+    title = quote_plus(title)
+
+    #API SETUP
+    url = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI"
+    headers = {
+    "X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+    "X-RapidAPI-Key": "e22598e71fmsh79ab05e94e3b5ffp1d6072jsn9b0382f88b54"
+    }
+    finalURL = url + '?autoCorrect=false&pageNumber=1&pageSize=1&q=' + title + '&safeSearch=true'
+
+    #REQUEST
+    resp = requests.get(finalURL, headers = headers)
+    imgResp = resp.json()
+
+    #RETURN URL
+    url = imgResp['value'][0]['thumbnail']
+
+    return url
+
 
 #CONVERSION OF CURRENCIES
 def convertCurr(amt):
@@ -48,9 +72,12 @@ def student():
 @app.route('/result', methods = ['POST', 'GET'], host="ibmappendly.herokuapp.com")
 def result():
    if request.method == 'POST':
-      apiInit()
+      #FORM RESULT
       result = request.form['Name']
+
+      #SCRAPE RESULTS
       result = scrape(result)
+
       title = result[0]
       price = result[1]
       rating = result[2]
@@ -58,10 +85,15 @@ def result():
       warranty = result[4]
       CPL = int(price)/int(warranty)
       CPL = int(CPL)
+
+      #API CALLS
+      apiConvert()
+      imgURL = apiImage(title)
+
       #CURRENCY CONVERSION
       p = convertCurr(price)
       c = convertCurr(CPL)
-      return render_template("index.html", title = title, price = price, rating = rating, value = value, warranty = warranty, CPL = CPL, pusd = p[0], pyen = p[1], peur = p[2], cusd = c[0], cyen = c[1], ceur = c[2])
+      return render_template("index.html", title = title, imgURL = imgURL, price = price, rating = rating, value = value, warranty = warranty, CPL = CPL, pusd = p[0], pyen = p[1], peur = p[2], cusd = c[0], cyen = c[1], ceur = c[2])
 
 if __name__ == '__main__':
     app.run(debug=True)
